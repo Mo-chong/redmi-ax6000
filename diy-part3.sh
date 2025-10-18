430c9493d9ab28ec087d42889e126a5135d3dd42
Mo-chong <95346951+Mo-chong@users.noreply.github.com>
2025-10-18 17:17:21 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -2,25 +2,20 @@
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
-# 清理掉旧的、冲突的 feed 定义
-sed -i '/kenzok8/d' feeds.conf.default
+# 清理可能存在的旧 feed 定义 (如果之前有修改过 feeds.conf.default)
+# sed -i '/kenzok8/d' feeds.conf.default # 保留这一行，如果你之前确实用了 kenzok8
 
-# 添加你需要的独立插件源
-# MosDNS
+# 添加 MosDNS 及其数据源
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-# AdGuardHome - immortalwrt/packages 中通常自带，无需额外添加
-# 如果编译时找不到 adguardhome，再取消下面这行的注释
-# svn export https://github.com/immortalwrt/packages/trunk/net/adguardhome feeds/packages/net/adguardhome
-
-# Argon 主题 (通常也自带，无需添加)
-# 如果需要最新版，可以取消注释
+# Argon 主题和配置 (如果默认 feeds 中没有或版本太旧，可以取消注释)
 # rm -rf feeds/luci/themes/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 其他你可能需要的插件，都用类似 'git clone' 的方式独立添加
-# 例如添加 Passwall (如果需要)
+# 如果你需要 Passwall 或其他来自第三方仓库的插件，在这里添加它们的 git clone 命令
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+
+echo "DIY script 3: Added custom packages."
