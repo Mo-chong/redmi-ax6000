0555a36a82234b2e435e166a9d8ee395d8971311
Mo-chong <95346951+Mo-chong@users.noreply.github.com>
2025-10-18 17:17:34 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2024 P3TERX [https://p3terx.com](https://p3terx.com)
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
-# Modify default IP (可选, 如果你需要改 IP)
+# 修改默认 IP (如果需要，取消注释并修改为你想要的 IP)
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 设置密码为空 (保留)
+# 设置密码为空 (保留和原来一致)
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/base-files/files/bin/config_generate
 
-# 其他所有内容都可以删除，因为我们不再使用 kenzok8 或其他复杂的插件源
+echo "DIY script 4: Applied custom system modifications."
